b752ad6a9083dfc3ffe6475228fbff5ea6838557
Tu nombre <tu-email@dominio.com>
2025-12-02 22:59:21 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exam-final-app',
+  webDir: 'www'
+};
+
+export default config;
